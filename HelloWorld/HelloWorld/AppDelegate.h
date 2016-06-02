d7e09cd62b88fbac3d04d4f2194471c068c61f0b
zhangwy <zhangwy@fun.tv>
2016-06-02 17:07:14 +0800

    init helloworld

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by zhangwy on 16/6/2.
+//  Copyright © 2016年 zhangwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by zhangwy on 16/6/2.
+//  Copyright © 2016年 zhangwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
