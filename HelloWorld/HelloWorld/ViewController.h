2e0271488822f978258220d0ba6b48fef65836bf
zhangwy <zhangwy@fun.tv>
2016-06-02 19:26:04 +0800

    监听事件

--- HelloWorld/HelloWorld/ViewController.h
+++ HelloWorld/HelloWorld/ViewController.h
@@ -8,8 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>{
+    IBOutlet UISegmentedControl * uiSegmented;
+    IBOutlet UILabel * uiSegmentedLabel;
+    IBOutlet UISwitch * uiSwitch;
+    IBOutlet UILabel * uiSwitchLabel;
+    IBOutlet UITextField *uiTextField;
+    IBOutlet UIButton *uiButtonText;
+    IBOutlet UILabel *uiTextLabel;
+}
 
+@property (nonatomic, retain) UISegmentedControl * uiSegmented;
+@property (nonatomic, retain) UILabel * uiSegmentedLabel;
+@property (nonatomic, retain) UISwitch * uiSwitch;
+@property (nonatomic, retain) UILabel * uiSwitchLabel;
+@property (nonatomic, retain) UITextField *uiTextField;
+@property (nonatomic, retain) UIButton *uiButtonText;
+@property (nonatomic, retain) UILabel *uiTextLabel;
 
 @end
 
--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+@synthesize uiSegmented, uiSegmentedLabel, uiSwitch, uiSwitchLabel, uiTextField, uiButtonText, uiTextLabel;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +24,27 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    if (textField == uiTextField) {
+        return YES;
+    }
+    return NO;
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    if (textField == uiTextField) {
+    }
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == uiTextField) {
+        [textField resignFirstResponder];
+        uiTextLabel.text = textField.text;
+        return YES;
+    }
+    return NO;
 }
 
 @end
